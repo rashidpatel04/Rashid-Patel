f4118543b6ea42aafef1a43a2dffae61dba2d3a7
rashidpatel04 <143613074+rashidpatel04@users.noreply.github.com>
2024-04-07 03:10:44 +0530

    Initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,13 @@
+
+setTimeout(function() {
+  $('.about-info-inner').typed({
+      strings: [
+          "<span>Hi, I am Rashid Patel</span>. As a data scientist, I specialize in extracting valuable insights from complex datasets to drive informed decision-making. With a strong background in statistical analysis and machine learning, I excel in developing predictive models to solve real-world problems. Currently I continue Learning Artificial Intelligence and Machine Learning With Government Polytechnic Mumbai (GPM). My Future Plan is to do M.Tech. in AIML.<br><br><table class='contact-info-table'><tr><td><strong>Birthday:</strong></td><td>04 August 2007</td></tr><tr><td><strong>City:</strong></td><td>Mumbai</td></tr><tr><td><strong>Phone:</strong></td><td>+91 9867723481</td></tr><tr><td><strong>Email:</strong></td><td><a href='mailto:rashidpatel9804@gmail.com'>rashidpatel9804@gmail.com</a></td></tr></table>"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+  });
+}, 100);
+
+
+
